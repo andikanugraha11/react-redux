090043479ca53990e50bb039ea32f0247f111e11
Andika Nugraha <m.andika.nugraha@gmail.com>
2018-01-01 20:16:19 +0700

    Bridge React Redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Main from './Main.js';
 import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {this.props.user.username}
+        <Main btnName="Ganti User" username={this.props.user.username} btnAction={()=>{this.props.setName("dika")}}/>
       </div>
     );
   }
@@ -30,7 +24,7 @@ const mapDispatchToProps= (dispatch) =>{
   return {
     setName : (name) =>{
       dispatch({
-        type :"SET_NAME",
+        type :"SET_USERNAME",
         payload : name
       })
     }
